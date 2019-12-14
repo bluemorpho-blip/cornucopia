03544f0d9ab44f5bfcd6905839bb436b58b884ab
Ronald Stephens <rstephens@bluemorpho.cc>
2019-12-14 20:07:11 +0000

    users_cotroller.rb edits

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,44 +1,52 @@
 class ItemsController < ApplicationController
 
   get "/items" do
-    redirect_if_not_logged_in
-    @items = Item.all
-    erb :'items/index'
+    if logged_in?
+      @items = Item.all
+      erb :'items/index'
+    else
+      redirect '/cornucopias'
+    end
   end
 
   get "/items/new" do
-    redirect_if_not_logged_in
-    @error_message = params[:error]
-    erb :'items/new'
+    if logged_in?
+      erb :'items/new'
+    else
+      redirect '/cornucopias'
+    end
   end
 
   get "/items/:id/edit" do
-    redirect_if_not_logged_in
-    @error_message = params[:error]
-    @item = Item.find(params[:id])
-    erb :'items/edit'
+    if logged_in?
+      @item = Item.find(params[:id])
+      erb :'items/edit'
+    else
+      redirect '/cornucopias'
+    end
   end
 
   post "/items/:id" do
-    redirect_if_not_logged_in
-    @item = Item.find(params[:id])
+    if logged_in?
+      @item = Item.find(params[:id])
     unless Item.valid_params?(params)
-      redirect "/items/#{@item.id}/edit?error=invalid item"
+      redirect to "/items/#{@item.id}/edit?error=invalid item"
     end
     @item.update(params.select{ |key| key == "name" || key == "cornucopias_id"} )
     redirect "/items/#{@item.id}"
   end
 
   get "/items/:id" do
-    redirect_if_not_logged_in
-    @item = Item.find(params[:id])
-    redirect "/items/show"
+    if logged_in?
+      @item = Item.find(params[:id])
+      redirect to '/items/show'
+    end
   end
 
   post "/items" do
-    redirect_if_not_logged_in
-    unless Item.valid_params?(params)
-      redirect "/itmes/new?error=invalid item"
+    if logged_in?
+      unless Item.valid_params?(params)
+        redirect "/itmes/new?error=invalid item"
     end
     Item.create(params)
     redirect "/items"
