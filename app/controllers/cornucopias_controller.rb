a9da2b0d375ba6dc8ce5d4baea2386575dff9a8d
Ronald Stephens <rstephens@bluemorpho.cc>
2019-12-14 19:42:34 +0000

    edit to cornucpoia controller

--- app/controllers/cornucopias_controller.rb
+++ app/controllers/cornucopias_controller.rb
@@ -1,15 +1,37 @@
 class CornucopiasController < ApplicationController
 
   get "/cornucopias" do
-    redirect_if_not_logged_in
-    @cornucopias = Cornucopia.all
-    erb :'cornucopias/index'
+    if logged_in?
+      @cornucopias = Cornucopia.all
+      erb :'cornucopias/index'
+    else
+      redirect to '/login'
+    end
   end
 
   get "/cornucopias/new" do
-    redirect_if_not_logged_in
-    @error_message = params[:error]
-    erb :'cornucopias/new'
+    if logged_in?
+      erb :'cornucopias/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+
+    post "/cornucopias" do
+    if logged_in?
+      if params[:name] == ""
+        redirect to "/cornucpoias/new"
+      else
+        @cornucopia = current_user.cornucopias.build(name: params[:name])
+        if @cornucopia.save
+          redirect to "/cornucopias/#{@cornucopia.id}"
+        else
+          redirect to "/cornucopias/new"
+        end
+      end
+      redirect to '/login'
+    end
   end
 
   get "/cornucopias/:id/edit" do
@@ -50,16 +72,6 @@ end
     redirect_if_not_logged_in
     @cornucopia = Cornucopia.find(params[:id])
     erb :'cornucopias/show'
-  end
-
-  post "/cornucopias" do
-    redirect_if_not_logged_in
-
-    unless Cornucopia.valid_params?(params)
-      redirect "/cornucopias/new?error=invalid cornucopia"
-    end
-    Cornucopia.create(params)
-    redirect "/cornucopias"
   end
 
   delete "/cornucopias/:id/delete" do
